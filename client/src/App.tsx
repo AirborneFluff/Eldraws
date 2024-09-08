64fa64faf6d6146fa61521d66e50e9466285549c
AirborneFluff <proffitt.m.98@gmail.com>
2024-09-08 12:53:59 +0100

    Initial Client layout

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,138 @@
-import './App.css'
-import { GlassContainer } from './components/GlassContainer.tsx';
-import { DiscordButton } from './components/DiscordButton.tsx';
-import { Button } from './components/Button.tsx';
+import type { LoaderFunctionArgs } from "react-router-dom";
+import {
+  Form,
+  RouterProvider,
+  createBrowserRouter,
+  redirect,
+  useActionData,
+  useLocation,
+  useNavigation,
+} from "react-router-dom";
+import { fakeAuthProvider } from "./auth";
+import { LayoutUI } from './core/ui/LayoutUI.tsx';
 
-function App() {
-  console.log(import.meta.env.VITE_DISCORD_OAUTH_URL)
+const router = createBrowserRouter([
+  {
+    id: "root",
+    path: "/",
+    loader() {
+      return { user: fakeAuthProvider.username };
+    },
+    Component: LayoutUI,
+    children: [
+      {
+        index: true,
+        Component: PublicPage,
+      },
+      {
+        path: "login",
+        action: loginAction,
+        loader: loginLoader,
+        Component: LoginPage,
+      },
+      {
+        path: "protected",
+        loader: protectedLoader,
+        Component: ProtectedPage,
+      },
+    ],
+  },
+  {
+    path: "/logout",
+    async action() {
+      // We signout in a "resource route" that we can hit from a fetcher.Form
+      await fakeAuthProvider.signout();
+      return redirect("/");
+    },
+  },
+]);
+
+export default function App() {
   return (
-    <div className='py-72 px-4 min-h-screen'>
-      <DiscordButton href={import.meta.env.VITE_DISCORD_OAUTH_URL} />
-    </div>
-  )
+    <RouterProvider router={router} fallbackElement={<p>Initial Load...</p>} />
+  );
 }
 
-export default App
+async function loginAction({ request }: LoaderFunctionArgs) {
+  let formData = await request.formData();
+  let username = formData.get("username") as string | null;
 
+  // Validate our form inputs and return validation errors via useActionData()
+  if (!username) {
+    return {
+      error: "You must provide a username to log in",
+    };
+  }
+
+  // Sign in and redirect to the proper destination if successful.
+  try {
+    await fakeAuthProvider.signin(username);
+  } catch (error) {
+    // Unused as of now but this is how you would handle invalid
+    // username/password combinations - just like validating the inputs
+    // above
+    return {
+      error: "Invalid login attempt",
+    };
+  }
+
+  let redirectTo = formData.get("redirectTo") as string | null;
+  return redirect(redirectTo || "/");
+}
+
+async function loginLoader() {
+  if (fakeAuthProvider.isAuthenticated) {
+    return redirect("/");
+  }
+  return null;
+}
+
+function LoginPage() {
+  let location = useLocation();
+  let params = new URLSearchParams(location.search);
+  let from = params.get("from") || "/";
+
+  let navigation = useNavigation();
+  let isLoggingIn = navigation.formData?.get("username") != null;
+
+  let actionData = useActionData() as { error: string } | undefined;
+
+  return (
+    <div>
+      <p>You must log in to view the page at {from}</p>
+
+      <Form method="post" replace>
+        <input type="hidden" name="redirectTo" value={from} />
+        <label>
+          Username: <input name="username" />
+        </label>{" "}
+        <button type="submit" disabled={isLoggingIn}>
+          {isLoggingIn ? "Logging in..." : "Login"}
+        </button>
+        {actionData && actionData.error ? (
+          <p style={{ color: "red" }}>{actionData.error}</p>
+        ) : null}
+      </Form>
+    </div>
+  );
+}
+
+function PublicPage() {
+  return <h3>Public</h3>;
+}
+
+function protectedLoader({ request }: LoaderFunctionArgs) {
+  // If the user is not logged in and tries to access `/protected`, we redirect
+  // them to `/login` with a `from` parameter that allows login to redirect back
+  // to this page upon successful authentication
+  if (!fakeAuthProvider.isAuthenticated) {
+    let params = new URLSearchParams();
+    params.set("from", new URL(request.url).pathname);
+    return redirect("/login?" + params.toString());
+  }
+  return null;
+}
+
+function ProtectedPage() {
+  return <h3>Protected</h3>;
+}
