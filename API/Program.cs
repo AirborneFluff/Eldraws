30afb47c150e62362e858d4769cd52194769ebe1
AirborneUnitcat <flanagan.a.97@gmail.com>
2024-09-15 18:07:32 +0100

    Fixed issue with production deployment of SPA

--- API/Program.cs
+++ API/Program.cs
@@ -24,24 +24,5 @@ app.MapFallbackToController("Index", "Fallback");
 app.UseAuthentication();
 app.UseAuthorization();
 
-if (!app.Environment.IsDevelopment())
-{
-    app.UseSpaStaticFiles();
-}
-
-/*app.UseSpa(spa =>
-{
-    spa.Options.SourcePath = "./client";
-
-    if (app.Environment.IsDevelopment())
-    {
-        spa.UseProxyToSpaDevelopmentServer("http://localhost:5173/");
-    }
-    else
-    {
-        spa.Options.SourcePath = "./client/dist";
-    }
-});*/
-
 app.MapControllers();
 app.Run();
