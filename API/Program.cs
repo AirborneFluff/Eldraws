36334cd6611ec30532ce6e804c762db25053bca9
AirborneUnitcat <flanagan.a.97@gmail.com>
2024-09-15 18:21:04 +0100

    Fix deploy build

--- API/Program.cs
+++ API/Program.cs
@@ -5,24 +5,23 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.ConfigureServices();
+
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
-} 
+}
 
 app.UseHttpsRedirection();
-
-app.UseDefaultFiles();
 app.UseStaticFiles();
 
 app.MapControllers();
-app.MapFallbackToController("Index", "Fallback");
+
+app.MapFallbackToFile("index.html");
 
 app.UseAuthentication();
 app.UseAuthorization();
 
-app.MapControllers();
 app.Run();
