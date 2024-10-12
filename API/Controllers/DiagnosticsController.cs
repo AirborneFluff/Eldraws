7696301e0250f5b17ae8fc54acf6ae3e247ac419
AirborneFluff <proffitt.m.98@gmail.com>
2024-10-12 10:51:55 +0100

    Added stress endpoint

--- API/Controllers/DiagnosticsController.cs
+++ API/Controllers/DiagnosticsController.cs
@@ -1,12 +1,22 @@
+using API.Data;
+using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers;
 
-public class DiagnosticsController : BaseApiController
+public class DiagnosticsController(UnitOfWork unitOfWork, IMapper mapper) : BaseApiController
 {
     [HttpGet]
     public ActionResult Ping()
     {
         return Ok("Pong");
+    }
+
+    [HttpGet("stress/events/{eventId}/bingo")]
+    public async Task<ActionResult> GetBingoTilesStress(string eventId)
+    {
+        var bingoEvent = await unitOfWork.EventRepository.GetBingoEventByEventId(eventId);
+        var tiles = await unitOfWork.EventRepository.GetBingoBoardTiles(bingoEvent.Id);
+        return Ok();
     }
 }
