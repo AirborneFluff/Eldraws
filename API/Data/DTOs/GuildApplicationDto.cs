af19b15efc067d68f44121b7edfc9c49cc934706
AirborneFluff <proffitt.m.98@gmail.com>
2024-10-28 21:53:45 +0000

    GuildDto patch

--- API/Data/DTOs/GuildApplicationDto.cs
+++ API/Data/DTOs/GuildApplicationDto.cs
@@ -1,9 +1,14 @@
-﻿namespace API.Data.DTOs;
+﻿using API.Entities;
+
+namespace API.Data.DTOs;
 
 public class GuildApplicationDto
 {
     public required string Id { get; set; }
     public required string AppUserId { get; set; }
+
+    public required string GuildId { get; set; }
+    public GuildDto? Guild { get; set; }
     
     public required string UserName { get; set; }
     public string? Gamertag { get; set; }
